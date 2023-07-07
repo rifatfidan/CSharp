a72d31a285e807a3348394c8aed291428e293698
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-07-07 13:14:46 +0300

    Create day-001.cs
    Console.WriteLine()
    Comments
    Console.ReadLine()

---
+++ Day-001/day-001.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Project01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment. Single-line comments start with two forward slashes 
+            /* Multi-line comments start with
+            and ends with */
+
+            //You can add as many WriteLine() as you want. It will add a new line for each method
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
