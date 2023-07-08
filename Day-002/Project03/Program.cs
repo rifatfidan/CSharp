a65cdb684e0af51dbe109ad105371ca4930576bd
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-07-09 01:40:41 +0300

    Project03
    Project03

---
+++ Day-002/Project03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My C# Adventure"; // can change console title
+            Console.Write("Enter the first number : ");
+            //We convert from string to integer
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1 + number2;
+            Console.Write("The sum of the numbers : " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
