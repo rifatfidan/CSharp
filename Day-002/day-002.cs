865907be3d3f667759242ce468e3ad1329f24051
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-07-08 21:34:49 +0300

    Create day-002.cs
    Combine strings with + 
    Use variant and Readline()

---
+++ Day-002/day-002.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Project02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name; // create a string for Readline()
+            Console.Write("Input your name and surname please : ");
+            name = Console.ReadLine();
+            Console.Write("Your name is : " + name);// using + for merge variant and string
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
