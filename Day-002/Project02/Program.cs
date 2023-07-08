ae7116b35dc11b9774e8bff71ce7d84e32bff26f
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-07-08 23:40:48 +0300

    Project02
    Project02

---
+++ Day-002/Project02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name; // create a string for Readline()
+            Console.Write("Input your name and surname please : ");
+            name = Console.ReadLine();
+            Console.Write("Your name is : " + name);// we combine  string and variant with + 
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
