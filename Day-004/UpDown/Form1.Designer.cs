b25b3cb2c4fcff854575b861a974fe35739c948c
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-07-19 14:10:31 +0300

    Use buttons for increase and decrease result
    Use button for increase and decrease. And show in label live result.
    Use If-elseif...

---
+++ Day-004/UpDown/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace UpDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.lblDegree = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(23, 135);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(88, 40);
+            this.btnUp.TabIndex = 0;
+            this.btnUp.Text = "Increase";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(129, 135);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(88, 40);
+            this.btnDown.TabIndex = 1;
+            this.btnDown.Text = "Decrease";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // lblDegree
+            // 
+            this.lblDegree.AutoSize = true;
+            this.lblDegree.Font = new System.Drawing.Font("Segoe UI Semibold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDegree.Location = new System.Drawing.Point(49, 48);
+            this.lblDegree.Name = "lblDegree";
+            this.lblDegree.Size = new System.Drawing.Size(43, 50);
+            this.lblDegree.TabIndex = 2;
+            this.lblDegree.Text = "0";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResult.Location = new System.Drawing.Point(129, 64);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 30);
+            this.lblResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 201);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblDegree);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Name = "Form1";
+            this.Text = "Up and Down";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Label lblDegree;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Day-004/UpDown/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int degree=0;
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            degree++;
+            if (degree <= 3)
+            {
+                
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "Low";
+            }
+            else if (degree > 3 && degree < 5)
+            {
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "Normal";
+            }
+            else if (degree >= 5 && degree <= 8)
+            {
+             
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "High";
+            }
+            else if (degree > 8 && degree <= 10)
+            {
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "Very High";
+            }
+            else if (degree > 10)
+            {
+                degree = 10;
+                lblDegree.Text = degree.ToString();
+            }
+
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+
+            degree--;
+            if (degree < 0)
+            {
+                degree = 0;
+                lblDegree.Text = degree.ToString();
+            }
+            else if (degree <=3)
+            {
+                
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "Low";
+            }
+            else if (degree > 3 && degree < 5)
+            {
+                
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "Normal";
+            }
+            else if (degree >= 5 && degree <= 8)
+            {
+                
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "High";
+            }
+            else if (degree > 8 && degree <= 10)
+            {
+                
+                lblDegree.Text = degree.ToString();
+                lblResult.Text = "Very High";
+            }
+            
+        }
+    }
+}
