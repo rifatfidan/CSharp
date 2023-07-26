af942b185403563147252a9bc5f8f58234539c72
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-07-26 18:20:17 +0300

    FileSamples

---
+++ Day-006/FileSamples/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace FileSamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblauthor = new System.Windows.Forms.Label();
+            this.lblPublisher = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.lblListed = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNameList = new System.Windows.Forms.Label();
+            this.lblAuthorList = new System.Windows.Forms.Label();
+            this.lblPublisherList = new System.Windows.Forms.Label();
+            this.lblPriceList = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Split";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Write File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 192);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 302);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(125, 331);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(125, 360);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 23);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(125, 389);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(130, 23);
+            this.textBox5.TabIndex = 9;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(61, 305);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 15);
+            this.lblName.TabIndex = 10;
+            this.lblName.Text = "Name";
+            // 
+            // lblauthor
+            // 
+            this.lblauthor.AutoSize = true;
+            this.lblauthor.Location = new System.Drawing.Point(61, 334);
+            this.lblauthor.Name = "lblauthor";
+            this.lblauthor.Size = new System.Drawing.Size(44, 15);
+            this.lblauthor.TabIndex = 11;
+            this.lblauthor.Text = "Author";
+            // 
+            // lblPublisher
+            // 
+            this.lblPublisher.AutoSize = true;
+            this.lblPublisher.Location = new System.Drawing.Point(61, 363);
+            this.lblPublisher.Name = "lblPublisher";
+            this.lblPublisher.Size = new System.Drawing.Size(56, 15);
+            this.lblPublisher.TabIndex = 12;
+            this.lblPublisher.Text = "Publisher";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(72, 392);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(33, 15);
+            this.lblPrice.TabIndex = 13;
+            this.lblPrice.Text = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightGray;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Book Register                       ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightGray;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(285, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Registered Books";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(61, 430);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(180, 430);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(75, 23);
+            this.btnList.TabIndex = 17;
+            this.btnList.Text = "List-1";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // lblListed
+            // 
+            this.lblListed.AutoSize = true;
+            this.lblListed.Location = new System.Drawing.Point(285, 302);
+            this.lblListed.Name = "lblListed";
+            this.lblListed.Size = new System.Drawing.Size(38, 15);
+            this.lblListed.TabIndex = 18;
+            this.lblListed.Text = "Listed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightGray;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(521, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 25);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Registered Books";
+            // 
+            // lblNameList
+            // 
+            this.lblNameList.AutoSize = true;
+            this.lblNameList.Location = new System.Drawing.Point(521, 305);
+            this.lblNameList.Name = "lblNameList";
+            this.lblNameList.Size = new System.Drawing.Size(39, 15);
+            this.lblNameList.TabIndex = 20;
+            this.lblNameList.Text = "Name";
+            // 
+            // lblAuthorList
+            // 
+            this.lblAuthorList.AutoSize = true;
+            this.lblAuthorList.Location = new System.Drawing.Point(599, 305);
+            this.lblAuthorList.Name = "lblAuthorList";
+            this.lblAuthorList.Size = new System.Drawing.Size(44, 15);
+            this.lblAuthorList.TabIndex = 21;
+            this.lblAuthorList.Text = "Author";
+            // 
+            // lblPublisherList
+            // 
+            this.lblPublisherList.AutoSize = true;
+            this.lblPublisherList.Location = new System.Drawing.Point(697, 305);
+            this.lblPublisherList.Name = "lblPublisherList";
+            this.lblPublisherList.Size = new System.Drawing.Size(56, 15);
+            this.lblPublisherList.TabIndex = 22;
+            this.lblPublisherList.Text = "Publisher";
+            // 
+            // lblPriceList
+            // 
+            this.lblPriceList.AutoSize = true;
+            this.lblPriceList.Location = new System.Drawing.Point(790, 302);
+            this.lblPriceList.Name = "lblPriceList";
+            this.lblPriceList.Size = new System.Drawing.Size(33, 15);
+            this.lblPriceList.TabIndex = 23;
+            this.lblPriceList.Text = "Price";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(180, 471);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "List-2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 593);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblPriceList);
+            this.Controls.Add(this.lblPublisherList);
+            this.Controls.Add(this.lblAuthorList);
+            this.Controls.Add(this.lblNameList);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblListed);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblPublisher);
+            this.Controls.Add(this.lblauthor);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblauthor;
+        private System.Windows.Forms.Label lblPublisher;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Label lblListed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNameList;
+        private System.Windows.Forms.Label lblAuthorList;
+        private System.Windows.Forms.Label lblPublisherList;
+        private System.Windows.Forms.Label lblPriceList;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Day-006/FileSamples/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSamples
+{
+    public partial class Form1 : Form
+    {
+        public void Listed()
+        {
+            lblNameList.Text = "";
+            lblAuthorList.Text = "";
+            lblPublisherList.Text = "";
+            lblPriceList.Text = "";
+
+            StreamReader sr = new StreamReader("C:\\PYTHON\\book.txt");
+            string oku2 = sr.ReadLine();
+            while (oku2 != null)
+            {
+                string[] parcala2 = oku2.Split('$');
+                //Method 1
+                //foreach (var item in parcala2)
+                //{
+                //    lblListed.Text += item + " ";
+                //}
+                //lblListed.Text += "\n";
+
+                //Method 2
+                lblNameList.Text += parcala2[0] + "\n";
+                lblAuthorList.Text += parcala2[1] + "\n";
+                lblPublisherList.Text += parcala2[2] + "\n";
+                lblPriceList.Text += parcala2[3] + "\n";
+
+                oku2 = sr.ReadLine();
+
+            }
+            sr.Close();
+            sr.Dispose();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            //for use streamreader  we add system.io
+            //we give path but use \ again for not error
+            StreamReader sr = new StreamReader("C:\\PYTHON\\file.txt");
+            string oku = sr.ReadLine();
+            label1.Text = oku + "\n \n"; ;
+            //for read multiline from file
+            while (oku!=null)
+            {
+                label1.Text += oku + "\n";
+                // for read one line again
+                oku = sr.ReadLine();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            button2.Visible = false;
+            StreamReader sr = new StreamReader("C:\\PYTHON\\courses.txt");
+            string oku = sr.ReadLine();
+            
+            while (oku != null)
+            {
+                string[] parcala = oku.Split('$');
+                //foreach (var item in parcala)
+                //{
+                //    label1.Text += item + "\n";
+                //}
+                for (int i = 0; i < parcala.Length; i++)
+                {
+                    label1.Text += i+1 + ". kurs...............: " + parcala[i] + "\n";
+                }
+                
+                oku = sr.ReadLine();
+                sr.Close(); //file must close
+                sr.Dispose(); //all source close
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = File.AppendText("C:\\PYTHON\\cities.txt");
+            sw.WriteLine(textBox1.Text);
+            textBox1.Text = "";
+            sw.Close(); 
+            sw.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = File.AppendText("C:\\PYTHON\\book.txt");
+            string kayit = textBox2.Text + "$" + textBox3.Text + "$" + textBox4.Text + "$" + textBox5.Text;
+            sw.WriteLine(kayit);
+            sw.Close();
+            sw.Dispose();
+
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+            Listed();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lblListed.Text = "";
+            StreamReader sr = new StreamReader("C:\\PYTHON\\book.txt");
+            string oku2 = sr.ReadLine();
+            while (oku2!=null)
+            {
+                string[] parcala2 = oku2.Split('$');
+                //Method 1
+                foreach (var item in parcala2)
+                {
+                    lblListed.Text += item + " ";
+                }
+                lblListed.Text += "\n";
+                oku2 = sr.ReadLine();
+
+            }
+            sr.Close();
+            sr.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Listed();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listed();
+        }
+    }
+}
