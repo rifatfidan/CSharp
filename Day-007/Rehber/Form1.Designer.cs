3477c38ba986b843de2306da4c40404f69e3f9cd
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2023-08-18 17:04:29 +0300

    Solution Files of Rehber

---
+++ Day-007/Rehber/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿
+namespace Rehber
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.txtWeb = new System.Windows.Forms.TextBox();
+            this.lblweb = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFax = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTel2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTel1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSirketUnvan = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdbSirket = new System.Windows.Forms.RadioButton();
+            this.rdbSahis = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            this.groupBox1.Controls.Add(this.btnTemizle);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtAdres);
+            this.groupBox1.Controls.Add(this.lblAdres);
+            this.groupBox1.Controls.Add(this.txtWeb);
+            this.groupBox1.Controls.Add(this.lblweb);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtFax);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtTel2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtTel1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSirketUnvan);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rdbSirket);
+            this.groupBox1.Controls.Add(this.rdbSahis);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 492);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rehber Kaydı";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Seçili Satırı Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(93, 406);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 22;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click_1);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(172, 406);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(75, 23);
+            this.btnTemizle.TabIndex = 21;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(63, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(24, 15);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Tür";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(94, 311);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(153, 89);
+            this.txtAdres.TabIndex = 19;
+            // 
+            // lblAdres
+            // 
+            this.lblAdres.AutoSize = true;
+            this.lblAdres.Location = new System.Drawing.Point(12, 314);
+            this.lblAdres.Name = "lblAdres";
+            this.lblAdres.Size = new System.Drawing.Size(37, 15);
+            this.lblAdres.TabIndex = 18;
+            this.lblAdres.Text = "Adres";
+            // 
+            // txtWeb
+            // 
+            this.txtWeb.Location = new System.Drawing.Point(94, 282);
+            this.txtWeb.Name = "txtWeb";
+            this.txtWeb.Size = new System.Drawing.Size(153, 23);
+            this.txtWeb.TabIndex = 17;
+            // 
+            // lblweb
+            // 
+            this.lblweb.AutoSize = true;
+            this.lblweb.Location = new System.Drawing.Point(12, 285);
+            this.lblweb.Name = "lblweb";
+            this.lblweb.Size = new System.Drawing.Size(31, 15);
+            this.lblweb.TabIndex = 16;
+            this.lblweb.Text = "Web";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(94, 253);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(153, 23);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Email";
+            // 
+            // txtFax
+            // 
+            this.txtFax.Location = new System.Drawing.Point(94, 224);
+            this.txtFax.Name = "txtFax";
+            this.txtFax.Size = new System.Drawing.Size(153, 23);
+            this.txtFax.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Fax";
+            // 
+            // txtTel2
+            // 
+            this.txtTel2.Location = new System.Drawing.Point(94, 195);
+            this.txtTel2.Name = "txtTel2";
+            this.txtTel2.Size = new System.Drawing.Size(153, 23);
+            this.txtTel2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Tel Cep";
+            // 
+            // txtTel1
+            // 
+            this.txtTel1.Location = new System.Drawing.Point(94, 166);
+            this.txtTel1.Name = "txtTel1";
+            this.txtTel1.Size = new System.Drawing.Size(153, 23);
+            this.txtTel1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tel İş";
+            // 
+            // txtSirketUnvan
+            // 
+            this.txtSirketUnvan.Location = new System.Drawing.Point(94, 137);
+            this.txtSirketUnvan.Name = "txtSirketUnvan";
+            this.txtSirketUnvan.Size = new System.Drawing.Size(153, 23);
+            this.txtSirketUnvan.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Şirket Ünvan";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(94, 108);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(153, 23);
+            this.txtSoyad.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(94, 79);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(153, 23);
+            this.txtAd.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad";
+            // 
+            // rdbSirket
+            // 
+            this.rdbSirket.AutoSize = true;
+            this.rdbSirket.Location = new System.Drawing.Point(181, 38);
+            this.rdbSirket.Name = "rdbSirket";
+            this.rdbSirket.Size = new System.Drawing.Size(54, 19);
+            this.rdbSirket.TabIndex = 1;
+            this.rdbSirket.TabStop = true;
+            this.rdbSirket.Text = "Şirket";
+            this.rdbSirket.UseVisualStyleBackColor = true;
+            this.rdbSirket.CheckedChanged += new System.EventHandler(this.rdbSirket_CheckedChanged);
+            // 
+            // rdbSahis
+            // 
+            this.rdbSahis.AutoSize = true;
+            this.rdbSahis.Location = new System.Drawing.Point(111, 38);
+            this.rdbSahis.Name = "rdbSahis";
+            this.rdbSahis.Size = new System.Drawing.Size(52, 19);
+            this.rdbSahis.TabIndex = 0;
+            this.rdbSahis.TabStop = true;
+            this.rdbSahis.Text = "Şahıs";
+            this.rdbSahis.UseVisualStyleBackColor = true;
+            this.rdbSahis.CheckedChanged += new System.EventHandler(this.rdbSahis_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 492);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Lavender;
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(259, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(825, 492);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(825, 492);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listelenen Kayıtlar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(819, 470);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 492);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Rehber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.TextBox txtWeb;
+        private System.Windows.Forms.Label lblweb;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFax;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSirketUnvan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdbSirket;
+        private System.Windows.Forms.RadioButton rdbSahis;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Day-007/Rehber/Form1.cs
@@ -0,0 +1,121 @@
+﻿using Rehber.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rehber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rdbSirket_CheckedChanged(object sender, EventArgs e)
+        {
+            txtAd.Enabled = false;
+            txtSoyad.Enabled = false;
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtSirketUnvan.Enabled = true;
+        }
+
+        private void rdbSahis_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSirketUnvan.Clear();
+            txtSirketUnvan.Enabled = false;
+            txtAd.Enabled = true;
+            txtSoyad.Enabled = true;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSirketUnvan.Clear();
+            txtSirketUnvan.Enabled = false;
+            Yukle();
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            Temizle();
+        }
+
+        private void Temizle()
+        {
+            //Control kontrol = txtAd;
+            //kontrol.Text = "";
+            //groupBox1.Controls.Clear(); bunu yazarsak tüm öğeler silinir.
+            foreach (Control item in groupBox1.Controls)
+            {
+                //item.Visible = false;
+                if (item.GetType() == typeof(TextBox))
+                {
+                    item.Text = "";
+                }
+            }
+        }
+
+        private void Yukle()
+        {
+            dataGridView1.DataSource = null;
+            Kisi model = new Kisi();
+            dataGridView1.DataSource = model.Get();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnKaydet_Click_1(object sender, EventArgs e)
+        {
+            Kisi model = new Kisi();
+            model.Ad = txtAd.Text;
+            model.Soyad = txtSoyad.Text;
+            model.SirketUnvan = txtSirketUnvan.Text;
+            //tur
+            if (rdbSahis.Checked)
+            {
+                model.Turu = 1;
+            }
+            else
+            {
+                model.Turu = 2;
+            }
+            model.Tel1 = txtTel1.Text;
+            model.Tel2 = txtTel2.Text;
+            model.Fax = txtFax.Text;
+            model.Email = txtEmail.Text;
+            model.KayitTarih = DateTime.Now;
+            model.Web = txtWeb.Text;
+            model.Adres = txtAdres.Text;
+
+            model.Insert(model);
+            MessageBox.Show("Kayıt Başarılı", "Rehber Kayıt İşlemi");
+            Temizle();
+            Yukle();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Delete();
+            Yukle();
+        }
+
+        private void Delete()
+        {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            var dosya = new List<string>(File.ReadAllLines("C:\\Users\\PYTHON\\source\\repos\\Rehber\\rehber.txt"));
+            dosya.RemoveAt(rowIndex);
+            File.WriteAllLines("C:\\Users\\PYTHON\\source\\repos\\Rehber\\rehber.txt",dosya.ToArray());        }
+    }
+}
